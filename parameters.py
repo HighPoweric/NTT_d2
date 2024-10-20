ff14bc351b7110e7cbba178dbd1ba7bcf235a35d
HighPoweric <highpoweric@gmail.com>
2024-10-20 20:08:09 -0300

    primeiro commit

---
+++ parameters.py
@@ -0,0 +1,13 @@
+###############################################
+##### Constantes com as Regras de Negocio #####
+###############################################
+
+## Limite de Valor de Saques
+LIMITE_VALOR_SAQUE = 500
+
+## Limite de Qauntidade de Saques
+LIMITE_QUANT_SAQUE = 3
+
+## Permitir Saques que negativem Saldo (emprestimo) 0 = Não, 1 = Sim
+BOL_SAQUE_ALEM_SALDO = 0
+
