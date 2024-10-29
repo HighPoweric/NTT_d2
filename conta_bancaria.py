4b08a64dada1ab63e4a76788c6de6cf191b08d3b
HighPoweric <highpoweric@gmail.com>
2024-10-29 16:03:30 -0300

    Adicionado funcoes de saldo

--- conta_bancaria.py
+++ conta_bancaria.py
@@ -36,8 +36,45 @@ class ContaBancaria:
             print(f"Depósito de R${valor} realizado com sucesso.")              
             break
 
-    def saque (self, valor):
-        pass
+    def saque (self):
+        saques_restantes = self.saques_restantes()
+        print(f"Seu saldo é de R${self.saldo} você tem {saques_restantes} saques restantes hoje")
+        #saimos se não tem mais saques restantes
+        if saques_restantes <= 0: return
+        #iniciamos a funcionalidade de saque
+        while True:
+            try:                
+                print("indique um valor para Saque ou digite 0 para cancelar")
+                valor = int(float(input("R$ ")) * 100)/100 #metodo patenteado do Eric para truncar em duas casas decimais
+            except ValueError: #valor nao numerico
+                print('Por favor digite um valor numérico válido')
+                continue
+
+            if valor < 0:  # Verifica se o valor é positivo
+                print("O valor do saque deve ser positivo.")
+                continue
+            #saindo do deposito
+            elif valor == 0: 
+                break
+            #verificando se ultrapassa o limite de saque
+            elif valor > parameters.LIMITE_VALOR_SAQUE:
+                print(f'Valor acima do seu limite de saque de R${parameters.LIMITE_VALOR_SAQUE}')
+                continue
+            #Verificando saldo para saque
+            elif parameters.BOL_SAQUE_ALEM_SALDO and valor > self.saldo:
+                print("O valor do saque não pode superar seu saldo.")
+                continue
+
+            #atualizando saldo, quantidade de saques e registrando a transacao
+            self.saldo -= valor
+            self.saques_efetuados += 1
+            self.transacoes.append({
+                'data': datetime.now().isoformat(timespec='seconds'),
+                'tipo':'saque',
+                'valor': valor
+                })
+            print(f"Saque de R${valor} realizado com sucesso.")              
+            break
 
     def extrato (self):
         if not self.transacoes:
